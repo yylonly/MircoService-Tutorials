ced54303ad31e0dbfa1fa27c36653d8ed291109e
yylonly <yylonly@gmail.com>
2018-08-21 23:51:14 +0800

    JAX-RS CDI JSON-P

---
+++ guide-rest-intro/start/src/main/java/io/openliberty/guides/rest/SystemApplication.java
@@ -0,0 +1,9 @@
+package io.openliberty.guides.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("System")
+public class SystemApplication extends Application {
+
+}
